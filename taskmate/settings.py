1118a9440525a202fcbc76e1541cacd6b0769f91
bsosa09 <bsosa09@yahoo.com>
2021-10-06 20:06:48 -0700

    settings.py changes

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -68,11 +68,22 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskmate',
+        'USER': 'bryan',
+        'PASSWORD': 'glendale1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
