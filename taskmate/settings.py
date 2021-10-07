4aab5d140a2d3c47573bb625651510c7dfc40c30
bsosa09 <bsosa09@yahoo.com>
2021-10-06 20:23:36 -0700

    update4

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,21 +1,18 @@
 
 from pathlib import Path
-import environ
 import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(SECRET_KEY= str,)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_@n$w3eo2kfm6bo=t3e7_g42#vc=8bvih$4c*6&9=7jlo^fs@u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
